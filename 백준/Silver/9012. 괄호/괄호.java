24c1a4330c7888bee8b25ee1bd7232a02d761406
MyangD <xghujjk@gmail.com>
2026-02-26 01:36:14 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,31 +1,36 @@
-import java.util.*;
+import java.io.*;
 
 class Main {
-	public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for(int i=0; i<T; i++) {
-        	int count = 0;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 수
+        
+        for (int i = 0; i < T; i++) {
+            int count = 0;
             boolean b = true;
-            String arr = sc.next();
-            for(int j=0; j<arr.length(); j++) {
-              char c = arr.charAt(j);
-              if(c == '(') {
-                  count++;
-              } else {
-                  count--;
-              }
-              if(count < 0) {
-                  b = false;
-                  break;
-              }
-          	}
-        	if(b && count == 0) {
-        		System.out.println("YES");
-        	} else {
-        		System.out.println("NO");
-        	}
-    	}
-    	sc.close();
-	}
+            String arr = br.readLine();
+            
+            for (int j = 0; j < arr.length(); j++) {
+                char c = arr.charAt(j);
+                
+                if (c == '(') {
+                    count++;
+                } else {
+                    count--;
+                }
+                
+                if (count < 0) {
+                    b = false;
+                    break;
+                }
+            }
+            
+            if (b && count == 0) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
 }
